751cf31a0a56d1148b36692814761fdb5c319152
Dhvanit Lakhtariya ⚡ <117103433+dvnt819@users.noreply.github.com>
2025-03-19 19:57:20 +0530

    solved MinimumOperationsToMakeBinaryArrayElementsEqualToOneI.py

---
+++ MinimumOperationsToMakeBinaryArrayElementsEqualToOneI.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def minOperations(self, nums):
+        n = len(nums)
+        cnt = 0
+        i = 2
+        while i < n:
+            if nums[i - 2] == 0:
+                nums[i - 2] ^= 1
+                nums[i - 1] ^= 1
+                nums[i] ^= 1
+                cnt += 1
+            i += 1
+        if 0 in nums:
+            return -1
+        return cnt
+        
