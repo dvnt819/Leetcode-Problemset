bbd4bddfda78b72fa03675783420d9b7749722ff
Dhvanit Lakhtariya ⚡ <117103433+dvnt819@users.noreply.github.com>
2025-03-30 20:59:08 +0530

    solved PartitionLables.py

---
+++ PartitionLables.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def partitionLabels(self, s):
+        n = len(s)
+        P = [-1] * 26
+        for i, c in enumerate(s):
+            x = ord(c) - 97
+            P[x] = i
+        pLen = []
+        start, end = 0, -1
+        for i, c in enumerate(s):
+            x = ord(c) - 97
+            end = max(end, P[x])
+            if i == end:
+                pLen.append(end - start + 1)
+                start = i + 1
+        return pLen
