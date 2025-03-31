00ca8a147c8cac112ae31f56f21b7fba73184e24
Dhvanit Lakhtariya ⚡ <117103433+dvnt819@users.noreply.github.com>
2025-03-31 08:55:22 +0530

    solved PutMarblesInBags

---
+++ PutMarblesInBags.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def putMarbles(self, weights, k):
+        if k == 1:
+            return 0
+        
+        pair_sums = []
+        for i in range(len(weights) - 1):
+            pair_sums.append(weights[i] + weights[i + 1])
+        
+        pair_sums.sort()
+        
+        min_score = sum(pair_sums[:k-1])
+        max_score = sum(pair_sums[-(k-1):])
+        
+        return max_score - min_score
+
+        
