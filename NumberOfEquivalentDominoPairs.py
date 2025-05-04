c878d737e1309bb04a4fc6292ef94d2027698166
Dhvanit Lakhtariya ⚡ <117103433+dvnt819@users.noreply.github.com>
2025-05-04 12:40:58 +0530

    Solved NumberOfEquivalentDominoPairs.py

---
+++ NumberOfEquivalentDominoPairs.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def numEquivDominoPairs(self, dominoes):
+        seen = [0] * 100
+        equal_pairs = 0
+        for a, b in dominoes:
+            val = a * 10 + b if a <= b else b * 10 + a
+            equal_pairs += seen[val]
+            seen[val] += 1
+        return equal_pairs
+        
